778511c9e4fbfee4366edeb476f8f5479e3e1199
Eric Githaiga <ericgithaiga007@gmail.com>
2023-12-02 23:40:27 +0300

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [word for word in word_list if self.is_anagram(word)]
+
+    def is_anagram(self, word):
+        return self.word.lower() != word.lower() and sorted(self.word.lower()) == sorted(word.lower())
+    
+    
--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,7 @@ from anagram import Anagram
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+    anagram = Anagram('diaper')
+
+    
